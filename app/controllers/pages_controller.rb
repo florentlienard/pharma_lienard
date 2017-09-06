069c8af6244dfbeca3ea23e5b93f7021586e550b
Florent Lienard <f.lienard@tbs-education.org>
2017-09-06 15:16:22 +0200

    pages creation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def services
+    # render template: "pages#service"
+  end
+  def gammes
+  end
+  def ordonnance
+  end
+  def contact
+  end
 end
