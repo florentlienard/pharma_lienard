d6672986e1afe2ca86d6b2cacad305306c6cc8e3
Florent Lienard <f.lienard@tbs-education.org>
2017-09-15 11:43:32 +0200

    quick win

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get 'gammes', to: 'pages#gammes'
   get 'ordonnance', to: 'pages#ordonnance'
   get 'contact', to: 'pages#contact'
-  mount Attachinary::Engine => "/attachinary"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
