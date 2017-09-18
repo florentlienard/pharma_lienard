0b2b59d6fd448661539d70973888cfa37df5bfba
Florent Lienard <f.lienard@tbs-education.org>
2017-09-18 19:44:04 +0200

    postmark

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserMailer.welcome(@user).deliver_now
       redirect_to root_path
     else
       render :new
