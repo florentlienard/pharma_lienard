958c1883055ebc85293b7647e634784069caa8d8
Florent Lienard <f.lienard@tbs-education.org>
2017-09-19 11:48:10 +0200

    attachinary

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :social_number)
+    params.require(:user).permit(:first_name, :last_name, :email, :social_number, :photo)
   end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
 require "attachinary/orm/active_record"  # <= Add this line
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
